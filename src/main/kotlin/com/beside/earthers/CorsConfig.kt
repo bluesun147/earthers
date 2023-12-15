4b2d174905d10ad7cab5ff154bb3c693d1ac5e89
bluesun147 <bluesun147@naver.com>
2023-12-15 17:50:51 +0900

    [Feat] CORS 설정

---
+++ src/main/kotlin/com/beside/earthers/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.beside.earthers
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600)
+            }
+        }
+    }
+}
--- src/main/kotlin/com/beside/earthers/EarthersApplication.kt
+++ src/main/kotlin/com/beside/earthers/EarthersApplication.kt
@@ -3,6 +3,10 @@ package com.beside.earthers
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 // db 정보 없을 경우에 실행
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
@@ -10,4 +14,13 @@ class EarthersApplication
 
 fun main(args: Array<String>) {
 	runApplication<EarthersApplication>(*args)
+}
+
+@Bean
+fun corsConfigurer(): WebMvcConfigurer {
+	return object : WebMvcConfigurer {
+		override fun addCorsMappings(registry: CorsRegistry) {
+			registry.addMapping("/**").allowedOriginPatterns("*")
+		}
+	}
 }
